3fca1e355e005e13c5fbc53330564f831952d01c
thannh2 <thanhdoduc113@gmail.com>
2025-12-29 04:39:14 +0700

    add online payments api

--- backend/app/api/payments.py
+++ backend/app/api/payments.py
@@ -1,15 +1,48 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from app.core.db import get_db
-from backend.app.schemas.payment_schema import PaymentCreateRequest, SePayTransactionSchema, SePayWebhookPayload
-from app.services.payment_service import PaymentService
+from backend.app.core.db import get_db
+from backend.app.schemas.payment import PaymentTransactionCreate, SePayWebhookPayload
+from backend.app.services.payment_service import PaymentService
 
+# Import Auth
+from app.api.auth import get_current_user
+from app.schemas.auth import TokenData
+from app.models.account import Account
 router = APIRouter()
 
-@router.post("/create-qr")
+@router.post("/create-qr", summary="Create QR")
 def create_qr_code(
-    payload: PaymentCreateRequest,
+    payload: PaymentTransactionCreate,
+    db: Session = Depends(get_db),
+    token_data: TokenData = Depends(get_current_user)
+):
+    
+    user = db.query(Account).filter(Account.username == token_data.username).first()
+    
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Không tìm thấy thông tin tài khoản"
+        )
+    
+    return PaymentService.create_qr_transaction(
+        db=db,
+        user_id=user.residentID,
+        bill_ids=payload.bill_ids
+    )
+
+@router.post("/sepay-webhook", summary="Get Webhook from SePay")
+def receive_sepay_webhook(
+    payload: SePayWebhookPayload,
     db: Session = Depends(get_db)
 ):
-    pass
+    data = payload.transaction
+
+    return PaymentService.process_sepay_webhook(
+        db=db,
+        content=data.transaction_content,      # Nội dung CK 
+        amount_in=float(data.amount_in),       # Số tiền nhận được
+        gateway_id=str(data.id),               # ID giao dịch phía SePay
+        transaction_date=data.transaction_date # Ngày giờ giao dịch
+    )
