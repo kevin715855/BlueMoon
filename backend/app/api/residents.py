22b7ae3b6402f82358fdb62f10890b0dc59ced78
thannh2 <thanhdoduc113@gmail.com>
2025-12-29 21:39:06 +0700

    add feature only accountant can use this api

--- backend/app/api/residents.py
+++ backend/app/api/residents.py
@@ -7,6 +7,7 @@ from backend.app.core.db import get_db
 from backend.app.models.resident import Resident
 from backend.app.schemas.resident import ResidentBase, ResidentCreate
 from backend.app.models.apartment import Apartment
+from backend.app.api.auth import get_current_accountant
 
 router = APIRouter()
 
@@ -14,7 +15,8 @@ router = APIRouter()
 def get_residents(
     skip: int = 0, 
     limit: int = 100, 
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_accountant = Depends(get_current_accountant)
 ):
     """Lấy danh sách cư dân"""
     residents = db.query(Resident).offset(skip).limit(limit).all()
@@ -23,7 +25,8 @@ def get_residents(
 @router.post("/add-new-resident", response_model=ResidentBase, status_code=status.HTTP_201_CREATED)
 def create_resident(
     resident_in: ResidentCreate, 
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_accountant = Depends(get_current_accountant)
 ):
     apartment = db.query(Apartment).filter(Apartment.apartmentID == resident_in.apartmentID).first()
     if not apartment:
