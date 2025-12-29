a46951ed8cbb1a577e6073292aff91cb68d14d67
thannh2 <thanhdoduc113@gmail.com>
2025-12-29 17:27:29 +0700

    add residents manager

---
+++ backend/app/api/residents.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from typing import List
+from sqlalchemy.exc import IntegrityError
+
+from backend.app.core.db import get_db
+from backend.app.models.resident import Resident
+from backend.app.schemas.resident import ResidentBase, ResidentCreate
+from backend.app.models.apartment import Apartment
+
+router = APIRouter()
+
+@router.get("/get-residents-data", response_model=List[ResidentBase])
+def get_residents(
+    skip: int = 0, 
+    limit: int = 100, 
+    db: Session = Depends(get_db)
+):
+    """Lấy danh sách cư dân"""
+    residents = db.query(Resident).offset(skip).limit(limit).all()
+    return residents
+
+@router.post("/residents", response_model=  ResidentBase, status_code=status.HTTP_201_CREATED)
+def create_resident(
+    resident_in: ResidentCreate, 
+    db: Session = Depends(get_db)
+):
+    apartment = db.query(Apartment).filter(Apartment.apartmentID == resident_in.apartmentID).first()
+    if not apartment:
+        raise HTTPException(status_code=400, detail="Căn hộ không tồn tại")
+
+    new_resident = Resident(**resident_in.dict())
+    db.add(new_resident)
+
+    try:
+        db.commit()
+        db.refresh(new_resident)
+        return new_resident
+        
+    except IntegrityError as e:
+        db.rollback()
+        
+        print(f"DEBUG ERROR: {e}") 
+        error_msg = str(e.orig)
+        if "FK_Resident_User" in error_msg:
+             raise HTTPException(
+                 status_code=400, 
+                 detail=f"Tài khoản '{resident_in.username}' chưa tồn tại. Vui lòng tạo tài khoản trước."
+             )
+        
+        raise HTTPException(status_code=400, detail=f"Lỗi Database: {error_msg}")
