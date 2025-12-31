adebf1d9df814dc7682f390f618034cef2d15c30
thannh2 <thanhdoduc113@gmail.com>
2025-12-31 11:08:07 +0700

    add user view bills

---
+++ backend/app/api/bills.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from typing import List
+
+from backend.app.core.db import get_db
+from backend.app.schemas.bill import BillRead
+from backend.app.models.bill import Bill
+from backend.app.models.resident import Resident 
+
+from backend.app.api.auth import get_current_user
+
+router = APIRouter()
+
+@router.get("/my-bills", response_model=List[BillRead])
+def get_bills_data(
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_user)
+):
+    resident = db.query(Resident).filter(current_user.username == Resident.username).first()
+    if not resident:
+        return []
+
+    my_bills = db.query(Bill).filter(Bill.apartmentID==resident.apartmentID).all()
+    return my_bills
+
