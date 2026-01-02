4cddde01e8c8b3b993cd03bed73af6a60f8b4aa2
thannh2 <thanhdoduc113@gmail.com>
2026-01-02 13:21:23 +0700

    add notification models

---
+++ backend/app/models/notification.py
@@ -0,0 +1,24 @@
+import datetime as dt
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Text, DECIMAL
+from backend.app.core.db import Base
+
+class Notification(Base):
+    __tablename__ = "notifications"
+
+    notificationID = Column(Integer, primary_key=True, index=True)
+    residentID = Column(Integer, ForeignKey("residents.residentID"), nullable=False)
+    
+    title = Column(String(200), nullable=False)
+    content = Column(Text, nullable=True)
+    
+    # Các type: 'METER_READING', 'NEW_BILL', 'PAYMENT_RESULT', 'GENERAL'
+    type = Column(String(50), nullable=False) 
+    
+    relatedID = Column(Integer, nullable=True) # ID hóa đơn hoặc ID giao dịch
+    isRead = Column(Boolean, default=False)    # Trạng thái đã xem
+    
+    createdDate = Column(DateTime, default=dt.datetime.now)
+
+    # Chỉ số điện nước
+    electricity = Column(DECIMAL(10, 2), nullable=True)
+    water = Column(DECIMAL(10, 2), nullable=True)
